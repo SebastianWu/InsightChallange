03b65a97d2e7133af362f0c5bc626ba6ea02c977
SebastianWu <32718422+SebastianWu@users.noreply.github.com>
2018-08-30 11:13:09 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
-python ./src/InsightChallange.py
+python src/InsightChallange.py input/actual.txt input/predicted.txt input/window.txt output/comparison.txt
